23465c156490af0efa7541f84400c27a4af11713
MostofaFattha <mostofacse14@gmail.com>
2023-11-16 09:07:56 +0600

    Another One

--- news-portsl.js
+++ news-portsl.js
@@ -6,6 +6,11 @@ const fetchCategories = () => {
 const showCategories=data=>{
      const categoriesContainer=document.getElementById('categories-container');
      data.news_category.forEach(singleCategory=>{
-        console.log(singleCategory);
+        // console.log(singleCategory);
+        // categoriesContainer.innerHTML+=`<a class="nav-link" href="#">${singleCategory.category_name}</a>`
+
+        let linkContainer=document.createElement('p');
+        linkContainer.innerHTML=`<a class="nav-link" href="#">${singleCategory.category_name}</a>`;
+        categoriesContainer.appendChild(linkContainer);
      });
 }
